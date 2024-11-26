ba2b4d28c4b9885d19b17fff6fd81a4fe317904a
Incx <94614160+Incx0@users.noreply.github.com>
2024-11-26 09:32:29 -0300

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -10,7 +10,9 @@ route.get("/",userController.get);
 route.get("/:id", userController.getById);
 //rota de post de um user no banco
 route.post("/add-user",userController.add);
-//rota de login de um user
+//rota de login de um admin
 route.post("/login", userController.login)
+//rota de login de um colaborador
+route.post("/login-colaborador", userController.loginColaborador)
 
 module.exports = route;
